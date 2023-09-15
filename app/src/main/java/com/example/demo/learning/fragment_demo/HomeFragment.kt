53a943f6e401249bf241ab14483c2d99e11f5ecb
panchalpriyank48@gmail.com <panchalpriyank48@gmail.com>
2023-09-15 17:47:52 +0530

    First Commit

---
+++ app/src/main/java/com/example/demo/learning/fragment_demo/HomeFragment.kt
@@ -0,0 +1,68 @@
+package com.example.demo.learning.fragment_demo
+
+import android.nfc.Tag
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      Log.d(TAG, "onCreate")
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "onCreateView")
+        return inflater.inflate(R.layout.fragment_home,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onStart")
+    }
+
+    companion object{
+        private const val TAG = "HomeFragment"
+    }
+    }
